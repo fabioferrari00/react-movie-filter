58c850cc44521b21de3e1b114b33d196cd910b9c
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-09-01 15:28:19 +0200

    [feat] end milestones

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 const initialFilms = [
   { title: 'Inception', genre: 'Fantascienza' },
@@ -9,22 +9,34 @@ const initialFilms = [
   { title: 'Pulp Fiction', genre: 'Thriller' },
 ]
 function App() {
+  const [films, setFilms] = useState(initialFilms)
+  const [genre, setGenre] = useState("");
+  const [filteredFilms, setFilteredFilms] = useState(initialFilms)
+
+  useEffect(() => {
+    console.log(`Trigger`);
+    const array = films.filter((film) => {
+      return film.genre.toLowerCase().includes(genre.toLowerCase())
+    });
+    setFilteredFilms(array);
+  }, [genre])
+
   return (
     <>
       <div className="container mt-3">
         <div className="row">
           <div className="col-12">
             <form className="mb-2">
-              <select name="" id="" >
+              <select value={genre} onChange={(e) => setGenre(e.target.value)} >
                 <option value="">Seleziona il genere</option>
                 <option value="thriller">Thriller</option>
-                <option value="romance">Romantico</option>
-                <option value="action">Azione</option>
-                <option value="fantasy">Fantascienza</option>
+                <option value="romantico">Romantico</option>
+                <option value="azione">Azione</option>
+                <option value="fantascienza">Fantascienza</option>
               </select>
             </form>
             <ul className="list-unstayled list-group">
-              {initialFilms.map((film) => {
+              {filteredFilms.map((film) => {
                 return (
                   <li className="list-group-item">
                     <h2>
